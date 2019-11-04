fb4164eda9f4946f6c779a0184e8c64f09c7ac18
Robert L Walton <walton@acm.org>
2019-11-04 06:13:38 -0500

    update

--- src/index.php
+++ src/index.php
@@ -1,17 +1,14 @@
 <?php
     session_start();
 
-    $EMAIL = "";
     $bad_confirm = false;
+    $begin_form =
+	'<form method="post" action="' .
+	$_SERVER['PHP_SELF'] . '">';
+    $end_form = '</form>';
 
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-	if ( array_key_exists ( 'email', $_REQUEST ) )
-	{
-	    $EMAIL = $_REQUEST['email'];
-	    $_SESSION['confirm'] = 
-	        bin2hex ( random_bytes ( 8 ) );
-	}
-	else if ( array_key_exists
+	if ( array_key_exists
 	              ( 'confirm', $_REQUEST ) )
 	{
 	    if (    $_SESSION['confirm']
@@ -24,30 +21,44 @@
 	    $_SESSION['confirm'] = 
 	        bin2hex ( random_bytes ( 8 ) );
 	}
+	else if ( array_key_exists
+	              ( 'email', $_REQUEST ) )
+	{
+	    $_SESSION['email'] = $_REQUEST['email'];
+	    $_SESSION['confirm'] = 
+	        bin2hex ( random_bytes ( 8 ) );
+	}
     }
 ?>
 <html>
 <body>
 
-<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
-  <?php if ( $EMAIL == "" )
+
+  <?php if ( $_SESSION['email'] == "" )
         {
+	    echo $begin_form;
 	    echo 'Email Address:' .
 	         ' <input type="text" name="email">';
+	    echo $end_form;
 	}
 	else
 	{
-	    if ( bad_confirm )
+	    if ( $bad_confirm )
 	    {
 	        echo 'CONFIRMATION NUMBER WAS WRONG;' .
 		     ' TRY AGAIN<br>';
 	    }
-	    echo 'Email Address: ' . $EMAIL . '<br>';
+	    echo 'Email Address: ' . $_SESSION['email']
+	         . '<br>';
+	    echo $begin_form;
 	    echo 'Confirmation Number:' .
 	         ' <input type="text" name="confirm">'
 		 . "<br>";
+	    echo $end_form;
+	    echo $begin_form;
 	    echo '<button name="email" value="">' .
 	         'Enter New Email Address</button>';
+	    echo $end_form;
 	    echo '<br>Confirmation Number is ' . 
 	         $_SESSION["confirm"];
 
