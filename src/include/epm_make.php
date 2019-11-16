4bd30dc06ecdb686699818f25477c75dd984780d
Robert L Walton <walton@acm.org>
2019-11-16 14:39:36 -0500

    update

--- src/include/epm_make.php
+++ src/include/epm_make.php
@@ -2,7 +2,7 @@
 
 // File:    epm_make.php
 // Author:  Robert L Walton <walton@acm.org>
-// Date:    Sat Nov 16 08:11:02 EST 2019
+// Date:    Sat Nov 16 12:25:29 EST 2019
 
 // Functions used to make files from other files.
 //
@@ -10,48 +10,47 @@
 // other special characters.
 
 // Given a problem name, a source file name, and a
-// template file name, determine if the template
-// file name matches the problem and source file
-// name.  If no, return NULL.  If yes, return
-// an array containing the map from wild card
-// symbols to their value.  Note that if template
-// does not contain PPPP or any other wildcard,
-// this may be an empty array.
+// template source name, determine if the template
+// source name matches the problem and source file
+// name.  If no, return NULL.  If yes, return an array
+// containing the map from wild card symbols to their
+// values.  Note that if the template does not contain
+// PPPP or any other wildcard, this may be an empty
+// array, but will not be NULL.
 //
-// If PPPP is in the template, replace it with
-// problem name before proceeding futher.
+// If PPPP is in the template, replace it with problem
+// name before proceeding futher.
 //
-function filename_match
+function template_match
     ( $problem, $filename, $template )
 {
-    if ( ! preg_match ( '/^([^:]*):/', $template,
-                                       $matches ) )
-        return NULL;
-    $temname = $matches[1];
-    $temname = preg_replace ( '/\./', '\\.', $temname );
-    $temname = preg_replace
-        ( '/PPPP/', $problem, $temname,
+    // Turn template into a regexp.
+    //
+    $template = preg_replace
+        ( '/\./', '\\.', $template );
+    $template = preg_replace
+        ( '/PPPP/', $problem, $template,
 	  -1, $PPPP_count );
     $offset = 0;
     $ids = [];
     while ( preg_match
-                ( '/[A-Z]/', $temname, $matches,
+                ( '/[A-Z]/', $template, $matches,
                   PREG_OFFSET_CAPTURE, $offset ) )
     {
         $char = $matches[0][0];
 	$offset = $matches[0][1];
 	if ( ! preg_match
-	           ( "/\G$char{4}/", $temname, $matches,
+	           ( "/\G$char{4}/", $template, $matches,
 		     0, $offset ) )
 	{
 	    ++ $offset;
 	    continue;
 	}
-	$temname = preg_replace
-	    ( "/$char{4}/", '(.*)', $temname, 1 );
+	$template = preg_replace
+	    ( "/$char{4}/", '(.*)', $template, 1 );
 	$ids[] = "$char$char$char$char";
     }
-    if ( ! preg_match ( "/^$temname\$/", $filename,
+    if ( ! preg_match ( "/^$template\$/", $filename,
                                          $matches ) )
         return NULL;
 
@@ -110,22 +109,33 @@ function substitute_match ( $item, $match )
 // source extension and destination extension (either
 // of which may be NULL if missing).  Return in
 // templates and options lists whose elements are
-// [filename, json-decode-of-file-contents] in the
-// order that the files were found.
+// [source, filename, json-decode-of-file-contents]
+// in the order that the files were found.
 //
-// All directory and file names are relative to epm_data.
+// While going through the directory list, find each
+// file with a required file extension and add it to
+// the requires list whose elements are just file names.
+//
+// The extension arguments, if not missing, are
+// regular expressions.  E.g., '(in|test|)' or just
+// 'cc'.  Required file extensions may NOT include
+// tmpl or optn.
+//
+// All directory and file names are relative to
+// epm_data.
 //
 // Any errors cause error messages to be appended to
 // the errors list.
 //
-function file_templates_and_options
-    ( $dirs, $src_ext, $des_ext,
-      & $templates, & $options, & $errors )
+function file_templates_options_and_requires
+    ( $dirs, $src_ext, $des_ext, $req_ext,
+      & $templates, & $options, & $requires, & $errors )
 {
     global $epm_data;
 
     $templates = [];
     $options = [];
+    $requires = [];
     foreach ( $dirs as $dir )
     {
         $desc = opendir ( "$epm_data/$dir" );
@@ -137,15 +147,34 @@ function file_templates_and_options
 	}
 	while ( $fname = readdir ( $desc ) )
 	{
+	    if ( preg_match ( '/\.([^.]*)$/', $fname, $matches ) )
+	        $ext = $matches[1];
+	    else
+	        $ext = "";
+
+	    if ( ! is_null ( $req_ext )
+	         &&
+	         preg_match ( "/^$req_ext$/", $ext ) )
+	    {
+	        $requires[] = "$dir/$fname";
+		continue;
+	    }
+
 	    if ( ! preg_match
-	           ( '/^([^:]+):([^:]+)(:.*|)' .
-		     '(\.tmpl|\.opt)$/',
-		     $fname, $matches ) )
+	               ( '/^(tmpl|optn)$/', $ext ) )
 	        continue;
+
+	    if ( ! preg_match
+	           ( '/^([^:]+):([^:]+):',
+		     $fname, $matches ) )
+	    {
+	        $errors[] = "bad template or option"
+		         . " file name format $fname";
+	        continue;
+	    }
 
 	    $src = $matches[0];
 	    $des = $matches[1];
-	    $type = $matches[3];
 
 	    if ( ! is_null ( $src_ext )
 		 &&
@@ -179,125 +208,73 @@ function file_templates_and_options
 		continue;
 	    }
 
-	    if ( $type == '.tmpl' )
-	        $templates[] = ["$dir/$fname", $json];
+	    if ( $ext == '.tmpl' )
+	        $templates[] =
+		    ["$src", "$dir/$fname", $json];
 	    else
-	        $options[] = ["$dir/$fname", $json];
+	    {
+	        $options[] =
+		    ["$src", "$dir/$fname", $json];
+	    }
 	}
 	close ( $desc );
     }
 }
 
-// Given the name of an uploaded file, and a list of
-// directories, find the template file that is to be
-// used to make something from the uploaded file, and
-// return its json decoded array with substitutions for
-// parameters in its file name.
+// Given the name of an uploaded file, and the tem-
+// plates, options, and requires lists from find_
+// templates_options_and_requires, return the template
+// file json data that is to be used to make something
+// from the uploaded file, with options inserted and
+// substitutions made for wildcards from the file
+// template.
 //
 // Require that the REQUIRES files of any returned
-// make control file exist or be the uploaded file.
+// template exist or be the uploaded file.
 //
-// If there are several suitable files, prefer first
-// ones with the largest number of REQUIRES files,
+// If there are several suitable template files, prefer
+// first ones with the largest number of REQUIRES files,
 // and second the one in the earliest directory in the
-// directory list.
-//
-// All directory names are relative to the global
-// $epm_data.
+// templates list.  If there are several suitable
+// options files, use the first one in the options list.
 //
 // Any errors cause error messages to be appended to
 // the errors list.
 //
-// Return the name of the best file found in best_
-// filename.
-//
 // If no file is found, return NULL.
 //
 function find_make_control
-	( $user_dir, $problem, $filename, $dirs,
-	  & $errors )
+	( $problem, $upload,
+	  $templates, $options, $requires, & $errors )
 {
-    global $epm_data;
-    $templates = [];
-        // List of json-decode-of-file for templates
-	// for which filename_match works, with wildcard
-	// matches substituted in the json.  Listed in
-	// order encountered.
-    $requires = [];
-        // Associative array of form [element => ""]
-	// for every element of a REQUIRES list in
-	// a template listed above.
-    foreach ( $dirs as $dir )
+    foreach ( $templates as $element )
     {
-        $desc = opendir ( "$epm_data/$dir" );
-	if ( ! $desc )
+        $template = $element[0];
+	$match = template_match
+	    ( $problem, $upload, $template );
+	if ( is_null ( $match ) ) continue;
+
+	$filename = $element[1];
+	$control = $element[2];
+
+	$control =
+	    substitute_match ( $control, $match );
+
+	if ( ! isset ( $control['REQUIRES'] ) )
 	{
 	    $errors[] =
-	        "cannot open search directory $dir";
+		"no REQUIRES in $filename";
 	    continue;
 	}
-	while ( $fname = readdir ( $desc ) )
+	$reqval = $control['REQUIRES'];
+	if ( ! is_array ( $reqval ) )
 	{
-	    if ( ! preg_match
-	               ( '/PPPP.*\.tmpl$/', $fname )
-		 &&
-		 ! preg_match
-	               ( "/$problem.*\.tmpl$/",
-		         $fname ) )
-	        continue;
-	    $match = filename_match
-                ( $problem, $filename, $fname );
-	    if ( is_null ( $match ) ) continue;
-	    $fileval =
-	        substitute_match ( $fileval, $match );
-	    if ( ! isset ( $fileval['REQUIRES'] ) )
-	    {
-		$errors[] =
-		    "no REQUIRES in $dir/$fname";
-		continue;
-	    }
-	    $reqval = $fileval['REQUIRES'];
-	    if ( ! is_array ( $reqval ) )
-	    {
-		$errors[] = "REQUIRES is not an array"
-		          . " in $dir/$fname";
-		continue;
-	    }
-
-	    $templates[] = $fileval;
-	    foreach ( $reqval as $required )
-	        $requires[$required] = "";
-	}
-	close ( $desc );
-    }
-
-    // Now go through the directories again, and for
-    // each file, if its name matches a key in
-    // $requires and that element of $requires has ""
-    // as its value, record the filename of the file
-    // relative to epm_data as he new value replacing
-    // the "".
-    //
-    foreach ( $dirs as $dir )
-    {
-        $desc = opendir ( "$epm_data/$dir" );
-	if ( ! $desc )
-	{
-	    $errors[] =
-	        "cannot open search directory $dir";
+	    $errors[] = "REQUIRES is not an"
+		      . " array in $filename";
 	    continue;
 	}
-	while ( $fname = readdir ( $desc ) )
-	{
-	    if ( isset ( $requires[$fname] )
-	         &&
-		 $requires[$fname] == ""
-		 &&
-		 is_readable
-		     ( "$epm_data/$dir/$fname" ) )
-	        $requires[$fname] = "$dir/$fname";
-	}
-	close ( $desc );
+
+	// TBD
     }
 }
 
