a14da4cfac1dbf5dd1076ec32b4e05b7a181367a
Robert L Walton <walton@acm.org>
2020-01-03 05:16:21 -0500

    update

--- tinker/crypto.php
+++ tinker/crypto.php
@@ -66,6 +66,8 @@ var iv = new Uint8Array ( 16 );
 var ivh = ArrayBuffer2hex ( iv );
 console.log ( "ivh = " + ivh );
 
+/*
+
 // WARNING: async/await is not currently (Jan 2020)
 // supported by Internet Explorer.  However Promises
 // are also NOT supported by Internet Explorer, which
@@ -90,6 +92,74 @@ async function compute_crypto()
 
 compute_crypto();
 // This runs compute_crypto as a separate task.
+
+*/
+
+// The following functions implement Internet Explorer
+// type interface with callbacks instead of Promises.
+//
+function IMPORTKEY
+	( format, keyData, algorithm, extractable,
+	  usages, callback )
+{
+    window.crypto.subtle.importKey
+        ( format, keyData, algorithm, extractable,
+	          usages )
+	.then ( callback );
+}
+
+function ENCRYPT ( algorithm, key, data, callback )
+{
+    window.crypto.subtle.encrypt
+        ( algorithm, key, data )
+	.then ( callback );
+}
+
+function DECRYPT ( algorithm, key, data, callback )
+{
+    window.crypto.subtle.decrypt
+        ( algorithm, key, data )
+	.then ( callback );
+}
+//
+// End of callback crypto functions.
+
+var kj, cjh, djh;
+
+function STEP1()
+{
+    IMPORTKEY
+	( "raw", hex2ArrayBuffer ( k ), "AES-CBC",
+	  true, ["encrypt", "decrypt"],
+	  STEP2 );
+}
+
+function STEP2 ( key )
+{
+    kj = key;
+    ENCRYPT
+        ( { name: "AES-CBC", iv }, kj,
+	  hex2ArrayBuffer ( m ),
+	  STEP3 );
+}
+
+function STEP3 ( encrypted )
+{
+    cjh = ArrayBuffer2hex ( encrypted );
+    console.log ( "cjh = " + cjh );
+    DECRYPT
+        ( { name: "AES-CBC", iv }, kj,
+	  hex2ArrayBuffer ( cjh ),
+	  STEP4 );
+}
+
+function STEP4 ( decrypted )
+{
+    djh = ArrayBuffer2hex ( decrypted );
+    console.log ( "djh = " + djh );
+}
+
+STEP1();
 
 
 </script>
