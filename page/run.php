42d9d6ad507aed12f5ffb8464c59d0e06b2bb2ee
Robert L Walton <walton@acm.org>
2020-02-29 05:58:25 -0500

    update

--- page/run.php
+++ page/run.php
@@ -2,7 +2,7 @@
 
     // File:	run.php
     // Author:	Robert L Walton <walton@acm.org>
-    // Date:	Sat Feb 29 04:32:55 EST 2020
+    // Date:	Sat Feb 29 05:58:07 EST 2020
 
     // Starts and monitors problem runs and displays
     // results.
@@ -456,6 +456,15 @@ EOT;
 	    list ( $id, $fname, $fcontents ) = $e;
 	    $fcontents = htmlspecialchars
 		( $fcontents );
+	    if ( preg_match ( '/\.rout$/', $fname ) )
+		$fcontents =
+		    preg_replace
+			( '/(?m)^(Score|' .
+			  'First-Failed-Test-Case):' .
+			  '.*$/',
+			  '</pre><pre class="red">$0' .
+			  "\n</pre><pre>",
+			  $fcontents );
 	    echo <<<EOT
 	    <div hidden id='$id' class='file'>
 	    <h5>$fname:</h5><br>
