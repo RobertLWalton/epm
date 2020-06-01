e223ab22ba691656d151de5124e3f9e93df0db4d
Robert L Walton <walton@acm.org>
2020-06-01 16:03:14 -0400

    update

--- tinker/tabs.php
+++ tinker/tabs.php
@@ -1,48 +1,51 @@
 <html>
 <body>
 
-Window Name: <span id='window-name'></span>
+Current Window Name: <span id='window-name'></span>
+&nbsp; &nbsp;
+<span id='ping' style='color:red'></span>
+<br>
+Name Argument: <input type='text' id='name' size='24'>
 <br>
 <button type='button' onclick='NEW_WINDOW()'>Make New Window</button>
-or <button type='button' onclick='CLOSE_WINDOW()'>Close Window</button>
-or <button type='button' onclick='SET_WINDOW()'>Set Window</button>
-with name <input type='text' id='name' size='24'>
 <br>
-<button type='button' onclick='SET()'>Set</button>
-session storage to value <input type='text' id='set' size='24'>
+<button type='button' onclick='CLOSE_WINDOW()'>Close Window</button>
 <br>
-<button type='button' onclick='GET()'>Get</button>
-session storage value: <span id='get'></span>
+<button type='button' onclick='PING_WINDOW()'>Ping Window</button>
+<br>
+<button type='button' onclick='CHANGE_NAME()'>Change Current Window Name</button>
 
 <script>
-// window.open will be ignored if not inside onclick function.
-//
 let window_name = document.getElementById ( 'window-name' );
 let name = document.getElementById ( 'name' );
-let set = document.getElementById ( 'set' );
-let get = document.getElementById ( 'get' );
+let ping = document.getElementById ( 'ping' );
 
-window_name.innerText = window.name;
+document.onvisibilitychange = function() {
+    if ( document.visibilityState != 'visible' )
+        return;
+    window_name.innerText = window.name;
+    if ( sessionStorage.getItem ( 'ping' ) != null )
+        ping.innerText = 'PINGED';
+    else
+        ping.innerText = '';
+    sessionStorage.removeItem ( 'ping' );
+}
 
 function NEW_WINDOW() {
     let w = window.open('/tinker/tabs.php', name.value, '' );
-    w.sessionStorage.setItem ( 'tabs', set.value );
 }
 function CLOSE_WINDOW() {
     let w = window.open('', name.value, '' );
     w.close();
 }
-function SET_WINDOW() {
+function PING_WINDOW() {
     let w = window.open('', name.value, '' );
-    w.sessionStorage.setItem ( 'tabs', set.value );
+    w.sessionStorage.setItem ( 'ping', '1' );
 }
-function SET()
+function CHANGE_NAME()
 {
-    sessionStorage.setItem ( 'tabs', set.value );
-}
-function GET()
-{
-    get.innerText = sessionStorage.getItem ( 'tabs' );
+    window.name = name.value;
+    window_name.innerText = window.name;
 }
 </script>
 
