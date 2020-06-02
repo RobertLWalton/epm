6216ed36a33ed560b7894a77b1c82332586c7847
Robert L Walton <walton@acm.org>
2020-06-02 16:38:25 -0400

    update

--- tinker/tabs.php
+++ tinker/tabs.php
@@ -8,6 +8,8 @@ Current Window Name: <span id='window-name'></span>
 &nbsp; &nbsp;
 <span id='ping' style='color:red'></span>
 <br>
+Opened by <span id='my-opener'>...</span>
+<br>
 Name Argument: <input type='text' id='name' size='24'>
 <br>
 <button type='button' onclick='NEW_WINDOW()'>Make New Window</button>
@@ -27,15 +29,31 @@ Name Argument: <input type='text' id='name' size='24'>
 &nbsp;&nbsp;&nbsp;
 The href of <span id='href-name'>...</span>
 is <span id='href-value'>...</span>
+<br>
+<button type='button' onclick='GET_OPENER()'>
+        GET OPENER</button>
+&nbsp;&nbsp;&nbsp;
+The name of the opener of <span id='opener-name'>...</span>
+is <span id='opener-value'>...</span>
 
 
 
 <script>
 let window_name = document.getElementById ( 'window-name' );
+let my_opener = document.getElementById ( 'my-opener' );
 let name = document.getElementById ( 'name' );
 let ping = document.getElementById ( 'ping' );
 let href_name = document.getElementById ( 'href-name' );
 let href_value = document.getElementById ( 'href-value' );
+let opener_name = document.getElementById ( 'opener-name' );
+let opener_value = document.getElementById ( 'opener-value' );
+
+my_opener.innerText = 'NONE;'
+if ( window.opener != null )
+    my_opener.innerText = window.opener.name;
+
+console.log ( 'WINDOW-NAME ' + window.name );
+window_name.innerText = window.name;
 
 document.onvisibilitychange = function() {
     if ( document.visibilityState != 'visible' )
@@ -71,6 +89,13 @@ function GET_HREF() {
     let w = window.open('', name.value, '' );
     href_name.innerText = name.value;
     href_value.innerText = w.location.href;
+}
+function GET_OPENER() {
+    let w = window.open('', name.value, '' );
+    opener_name.innerText = name.value;
+    opener_value.innerText = w.opener.name;
+    console.log ( 'OPENER ' + w.opener );
+    console.log ( 'OPENER NAME ' + w.opener.name );
 }
 // Note:  w = window.open ( ... ) forces focus to w
 //	  and window.focus() does nothing to
