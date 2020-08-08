64f9e2fa3665b843fff90176d755622fae244668
Robert L Walton <walton@acm.org>
2020-08-08 18:33:03 -0400

    update

--- tinker/upload.php
+++ tinker/upload.php
@@ -2,13 +2,20 @@
     echo '$_POST: '; print_r ( $_POST ); echo '<br>' . PHP_EOL;
     echo '$_FILES: '; print_r ( $_FILES ); echo '<br>' . PHP_EOL;
     $upload = $_FILES['uploaded_file'];
-    if ( $upload['error'] == 0 )
+    foreach ( $upload['error'] as $key => $error )
     {
-        $c = file_get_contents ( $upload['tmp_name'] );
-	echo 'SIZE: ' . strlen ( $c ) . '<br>' . PHP_EOL;
-	$c .= 'EOT' . PHP_EOL;
-	$c = htmlspecialchars ( $c );
-	echo "<pre>$c</pre>";
+        if ( $error == UPLOAD_ERR_NO_FILE ) continue; 
+	echo 'NAME: ' . $upload['name'][$key] . '<BR>';
+	echo 'TYPE: ' . $upload['type'][$key] . '<BR>';
+	echo 'SIZE: ' . $upload['size'][$key] . '<BR>';
+        if ( $error != UPLOAD_ERR_OK )
+	    echo 'ERROR: ' . $error . '<BR>';
+	else
+	{
+	    $c = file_get_contents ( $upload['tmp_name'][$key] );
+	    $c = htmlspecialchars ( $c );
+	    echo "CONTENTS:<BR><PRE>$c</PRE>";
+	}
     }
 ?>
     
@@ -20,7 +27,7 @@
 <input type="hidden" name="file" value='FILE'>
 <input type="hidden" name="more_files" value='MORE_FILES'>
 <input type="hidden" name="op" value='OP'>
-<input type="file" name="uploaded_file" title="file to upload">
+<input type="file" name="uploaded_file[]" title="files to upload" multiple>
 <button type="submit" name="A" value='A'>A</button>
 <button type="submit" name="B" value='B1'>B1</button>
 <button type="submit" name="B" value='B2'>B2</button>
